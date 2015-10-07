47fd84788db21be915f2a61f3175321987dc63d6
Mario Đurašin <indiche@gmail.com>
2015-10-07 10:58:08 +0200

    Initial commit

---
+++ ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    puts("Hello world.");
+    puts("This");
+    puts("is");
+    puts("me");
+    puts("trying");
+    puts("to");
+    puts("print");
+    puts("multiple");
+    puts("lines");
+
+    return 0;
+}
