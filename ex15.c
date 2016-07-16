0767d58b625adaa92e89b855a4d4c7c9a6bf3781
Mario Đurašin <mario.durasin@undabot.com>
2016-07-16 16:55:01 +0200

    Ex15, getting back to it

--- ex15.c
+++ ex15.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-    // create two array we care about
+    // create two arrays we care about
     int ages[] = { 23, 43, 12, 89, 2 };
     char *names[] = {
         "Alan", "Frank",
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     int i = 0;
 
     // first way using indexing
-    for(i = 0; i < count; i++) {
+    for (i = 0; i < count; i++) {
         printf("%s has %d years alive.\n", names[i], ages[i]);
     }
 
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 
     // second way using pointers
     for (i = 0; i < count; i++) {
-        printf("%s is %d years old.\n", *(cur_name+i), *(cur_age+i));
+        printf("%s is %d years old.\n", *(cur_name + i), *(cur_age + i));
     }
 
     printf("---\n");
@@ -39,8 +39,7 @@ int main(int argc, char *argv[])
     printf("---\n");
 
     // fourth way with pointers in a stupid complex way
-    for (cur_name = names, cur_age = ages; (cur_age - ages) < count; cur_name++, cur_age++)
-    {
+    for (cur_name = names, cur_age = ages; (cur_age - ages) < count; cur_name++, cur_age++) {
         printf("%s lived %d years so far.\n", *cur_name, *cur_age);
     }
 
